b2b1bb92fb8557d3e578b27de9acd626ee764ff9
Gustavo_lima07 <gl401062@gmail.com>
2025-04-21 19:26:39 -0300

    Projeto iniciado
    Ambiente vite + react adicionado e teste de componentes

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import  { Cabecalho  }   from './components';
+
+const App = () => {
+  return (
+    <>
+      <Cabecalho />
+    </>
+  )
+}
+
+export { App };
---
+++ vite-project/src/components/Cabacalho/Cabacalho.jsx
@@ -0,0 +1,12 @@
+import style from './Cabecalho.module.css';
+
+const Cabecalho = () => {
+    return (
+        <div className={style.Cabecalho}>
+            <h1>Hello Word!</h1>
+        </div>
+        
+    );
+};
+
+export { Cabecalho };    
