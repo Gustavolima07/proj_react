9268f847941249f15e7f8662f4249a9cb2ba62df
Gustavo_lima07 <gl401062@gmail.com>
2025-05-01 13:57:13 -0300

    Componente Slidebar Atualizado

--- vite-project/src/components/index.js
+++ vite-project/src/components/index.js
@@ -2,3 +2,4 @@ export * from "./NavBar";
 export * from "./Footer";
 export * from "./Conteudo";
 export * from "./Botao";
+export	* from "./SlideBar";
--- vite-project/src/pages/Inicial/Inicial.jsx
+++ vite-project/src/pages/Inicial/Inicial.jsx
@@ -1,48 +1,10 @@
-
+import { SlideBar} from "../../components/SlideBar";
 
 const Inicial = () => { 
     return (
         <div className="container">
             <div className="row d-flex justify-content-center">
-                <div className="col-11 ">
-                    <aside className="d-flex  mt-5">
-                        <div className="sidebar">
-                            <nav className="nav flex-column">
-                                <a href="#" className="nav-link">
-                                    <span className="icon">
-                                        <i class="bi bi-columns-gap">Dashboard</i>
-                                    </span>
-                                </a>
-                                <a href="#" className="nav-link">
-                                    <span className="description">
-                                    <i class="bi bi-bag">Product</i>
-                                    </span>
-                                </a>
-
-                                <a href="#" className="nav-link">
-                                    <span className="description">
-                                    <i class="bi bi-bell">Notificações</i>
-                                    </span>
-                                </a>
-                                <a href="#" className="nav-link" data-bs-toggle="collapse" data-bs-target="#sub-menu">
-                                    <span className="description">
-                                    <i class="bi bi-person"></i>
-                                    </span>
-                                    <span className="description">Login <i className="bi bi-caret-down"></i></span>
-                                </a>
-
-                                <div className="sub-menu collapse" id="sub-menu">
-                                    <a href="#" className="nav-link">
-                                        <span className="icon">
-                                            <i class="bi bi-person-plus"></i>
-                                        </span>
-                                    </a>
-                                </div>
-                            </nav>
-                        </div>
-                    </aside>
-
-                </div>
+                <SlideBar />
             </div>
         </div>
     );
