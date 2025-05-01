f93cc562d72a914f9a67322a4b608f396aefc50e
Gustavo_lima07 <gl401062@gmail.com>
2025-05-01 13:30:50 -0300

    Slidebar iniciado

--- vite-project/src/pages/Inicial/Inicial.jsx
+++ vite-project/src/pages/Inicial/Inicial.jsx
@@ -2,11 +2,45 @@
 
 const Inicial = () => { 
     return (
-        <div className="container mt-3">
-            <div className="row">
-                <div className="col-11 col-md-6 offset-md-3 text-center mt-5 mb-5">
-                    <h1>Bem-vindo à nossa página inicial!</h1>
-                    <p>Esta é a página inicial do nosso site.</p>
+        <div className="container">
+            <div className="row d-flex justify-content-center">
+                <div className="col-11 ">
+                    <aside className="d-flex  mt-5">
+                        <div className="sidebar">
+                            <nav className="nav flex-column">
+                                <a href="#" className="nav-link">
+                                    <span className="icon">
+                                        <i class="bi bi-columns-gap">Dashboard</i>
+                                    </span>
+                                </a>
+                                <a href="#" className="nav-link">
+                                    <span className="description">
+                                    <i class="bi bi-bag">Product</i>
+                                    </span>
+                                </a>
+
+                                <a href="#" className="nav-link">
+                                    <span className="description">
+                                    <i class="bi bi-bell">Notificações</i>
+                                    </span>
+                                </a>
+                                <a href="#" className="nav-link" data-bs-toggle="collapse" data-bs-target="#sub-menu">
+                                    <span className="description">
+                                    <i class="bi bi-person"></i>
+                                    </span>
+                                    <span className="description">Login <i className="bi bi-caret-down"></i></span>
+                                </a>
+
+                                <div className="sub-menu collapse" id="sub-menu">
+                                    <a href="#" className="nav-link">
+                                        <span className="icon">
+                                            <i class="bi bi-person-plus"></i>
+                                        </span>
+                                    </a>
+                                </div>
+                            </nav>
+                        </div>
+                    </aside>
 
                 </div>
             </div>
