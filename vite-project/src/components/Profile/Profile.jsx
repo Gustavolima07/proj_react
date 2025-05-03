e840723d33775bca115eb55ad274ff1b52236b53
Matheus <matheus.valerio6@etec.sp.gov.br>
2025-05-02 21:02:32 -0300

    #

--- vite-project/src/components/Profile/Profile.jsx
+++ vite-project/src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import './Profile.css';
 function Profile() {
   const [user, setUser] = useState({
     name: 'Marcos Cavalcanti',
-    email: 'marcus@gmail.com',
+    email: 'marcos@gmail.com',
     bio: 'Desenvolvedor apaixonado por tecnologia e café.',
     avatar: 'https://images.vexels.com/media/users/3/147101/isolated/preview/b4a49d4b864c74bb73de63f080ad7930-botao-de-perfil-do-instagram.png',
     isProfessional: false,
